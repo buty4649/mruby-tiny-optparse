9e7f9685b5caeb250243480b6310ce75ae1976ed
buty4649 <buty4649@gmail.com>
2025-07-21 16:43:51 +0900

    Refactor CI: remove unnecessary bundle install and gem install bundler steps

--- example/parse.rb
+++ example/parse.rb
@@ -1,24 +1,24 @@
 # This file works with both CRuby and mruby
 unless Object.const_defined?(:OptionParser)
-  require_relative "../mrblib/option_parser"
-  require_relative "../mrblib/option_parser/option"
-  require_relative "../mrblib/option_parser/switch"
-  require_relative "../mrblib/option_parser/list"
-  require_relative "../mrblib/option_parser/error"
+  require_relative '../mrblib/option_parser'
+  require_relative '../mrblib/option_parser/option'
+  require_relative '../mrblib/option_parser/switch'
+  require_relative '../mrblib/option_parser/list'
+  require_relative '../mrblib/option_parser/error'
 end
 
 opts = OptionParser.new
 
-opts.banner = "Usage: example [options]"
-opts.separator ""
+opts.banner = 'Usage: example [options]'
+opts.separator ''
 
-opts.on("--abc HOGE", "description of -a") {|v| puts "-a v: #{v}" }
-opts.on("--better", "description of --better") { puts "--better" }
-opts.on("--count=[number]", "description of --count") { |v| puts "--count v: #{v}" }
+opts.on('--abc HOGE', 'description of -a') { |v| puts "-a v: #{v}" }
+opts.on('--better', 'description of --better') { puts '--better' }
+opts.on('--count=[number]', 'description of --count') { |v| puts "--count v: #{v}" }
 
-opts.separator("--------------------------------")
+opts.separator('--------------------------------')
 
-opts.on("--[no-]debug", "description of --no-debug") { |v| puts "--no-debug #{v}" }
+opts.on('--[no-]debug', 'description of --no-debug') { |v| puts "--no-debug #{v}" }
 
 puts opts.parse(%w[--abc HOGE --better --count=100 --no-debug test])
 
