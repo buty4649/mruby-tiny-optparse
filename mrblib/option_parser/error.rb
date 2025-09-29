3e9bd9f988431ac3f249666b39407238c93d4641
buty4649 <buty4649@gmail.com>
2025-09-29 12:25:59 +0900

    Add error class tests for C-defined OptionParser exceptions

--- mrblib/option_parser/error.rb
+++
@@ -1,6 +0,0 @@
-class OptionParser
-  class ParseError < StandardError; end
-  class InvalidOption < ParseError; end
-  class MissingArgument < ParseError; end
-  class NeedlessArgument < ParseError; end
-end
